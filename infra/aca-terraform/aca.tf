4d6329992295c82a4ffb1010fb90628d4ca4bd7c
Kai Walter <kai.walter@outlook.de>
2022-11-12 16:14:01 +0000

    simple-js added with ACR managed identity

--- infra/aca-terraform/aca.tf
+++ infra/aca-terraform/aca.tf
@@ -4,13 +4,6 @@ resource "azapi_resource" "aca_env" {
   location  = azurerm_resource_group.rg.location
   name      = var.resourcePrefix
   tags      = local.tags
-  # identity {
-  #   type = "UserAssigned"
-  #   identity_ids = [
-  #     azurerm_user_assigned_identity.acr_pull_identity.id
-  #   ]
-  # }
-  # schema_validation_enabled = false
 
   body = jsonencode({
     properties = {
@@ -25,31 +18,60 @@ resource "azapi_resource" "aca_env" {
   })
 }
 
-resource "azapi_update_resource" "aca_env" {
-  type      = "Microsoft.App/managedEnvironments@2022-03-01"
-  name      = azapi_resource.aca_env.name
-  parent_id = azurerm_resource_group.rg.id
+resource "azapi_resource" "container_app" {
+  type                      = "Microsoft.App/containerApps@2022-03-01"
+  name                      = "simple-js"
+  parent_id                 = azurerm_resource_group.rg.id
+  location                  = azurerm_resource_group.rg.location
+  response_export_values    = ["*"]
+  schema_validation_enabled = false
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.acr_pull_identity.id]
+  }
 
   body = jsonencode({
     properties = {
-      "appLogsConfiguration" : {
-        "destination" : "log-analytics"
-        "logAnalyticsConfiguration" : {
-          "customerId" : azurerm_log_analytics_workspace.log.workspace_id
-          "sharedKey" : azurerm_log_analytics_workspace.log.primary_shared_key
+      managedEnvironmentId = azapi_resource.aca_env.id
+      configuration = {
+        activeRevisionsMode = "Single"
+        secrets             = []
+        ingress = {
+          external   = true
+          targetPort = 5001
+          transport  = "auto"
+        }
+        dapr = {
+          appId       = "simple-js"
+          appPort     = 5001
+          appProtocol = "http"
+          enabled     = true
+        }
+        registries = [{
+          server   = azurerm_container_registry.acr.login_server
+          identity = azurerm_user_assigned_identity.acr_pull_identity.id
+        }]
+      }
+      template = {
+        containers = [
+          {
+            name  = "simple-js"
+            image = "${azurerm_container_registry.acr.login_server}/simple-js:latest"
+            env = [
+            ]
+            resources = {
+              cpu    = ".25"
+              memory = ".5Gi"
+            }
+          }
+        ]
+        scale = {
+          minReplicas = 1
+          maxReplicas = 1
+          rules       = []
         }
       }
     }
-    identity = {
-      # "type" : "SystemAssigned"
-      "type" : "UserAssigned"
-      "userAssignedIdentities" : {
-        "${azurerm_user_assigned_identity.acr_pull_identity.principal_id}" : {}
-      }
-    }
   })
-
-  depends_on = [
-    azapi_resource.aca_env
-  ]
 }
