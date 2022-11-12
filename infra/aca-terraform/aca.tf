e50e5c5830fb0955b230ac20a6b936440adf4e07
Kai Walter <kai.walter@outlook.de>
2022-11-12 07:08:14 +0000

    TF ACA managed identity not working

--- infra/aca-terraform/aca.tf
+++ infra/aca-terraform/aca.tf
@@ -1,9 +1,16 @@
 resource "azapi_resource" "aca_env" {
-  type      = "Microsoft.App/managedEnvironments@2022-06-01-preview"
+  type      = "Microsoft.App/managedEnvironments@2022-03-01"
   parent_id = azurerm_resource_group.rg.id
   location  = azurerm_resource_group.rg.location
   name      = var.resourcePrefix
   tags      = local.tags
+  # identity {
+  #   type = "UserAssigned"
+  #   identity_ids = [
+  #     azurerm_user_assigned_identity.acr_pull_identity.id
+  #   ]
+  # }
+  # schema_validation_enabled = false
 
   body = jsonencode({
     properties = {
@@ -19,27 +26,30 @@ resource "azapi_resource" "aca_env" {
 }
 
 resource "azapi_update_resource" "aca_env" {
-  type      = "Microsoft.App/managedEnvironments@2022-06-01-preview"
+  type      = "Microsoft.App/managedEnvironments@2022-03-01"
   name      = azapi_resource.aca_env.name
   parent_id = azurerm_resource_group.rg.id
 
   body = jsonencode({
+    properties = {
+      "appLogsConfiguration" : {
+        "destination" : "log-analytics"
+        "logAnalyticsConfiguration" : {
+          "customerId" : azurerm_log_analytics_workspace.log.workspace_id
+          "sharedKey" : azurerm_log_analytics_workspace.log.primary_shared_key
+        }
+      }
+    }
     identity = {
-      "type" : "SystemAssigned"
+      # "type" : "SystemAssigned"
+      "type" : "UserAssigned"
+      "userAssignedIdentities" : {
+        "${azurerm_user_assigned_identity.acr_pull_identity.principal_id}" : {}
+      }
     }
   })
 
   depends_on = [
     azapi_resource.aca_env
-  ]
-}
-
-resource "azurerm_role_assignment" "aks_acr_assignment" {
-  principal_id                     = jsondecode(azapi_update_resource.aca_env.output).identity[0].principal_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-  depends_on = [
-    azapi_update_resource.aca_env
   ]
 }
--- infra/aca-terraform/acr.tf
+++ infra/aca-terraform/acr.tf
@@ -11,3 +11,16 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Standard"
   admin_enabled       = false
 }
+
+resource "azurerm_user_assigned_identity" "acr_pull_identity" {
+  name                = "${var.resourcePrefix}-aca"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_role_assignment" "aks_acr_assignment" {
+  principal_id                     = azurerm_user_assigned_identity.acr_pull_identity.principal_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
