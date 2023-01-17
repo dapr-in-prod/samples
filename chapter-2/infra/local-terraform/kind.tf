d46bdd35dd2c9c282d0707255e6371652a81e208
Artur Souza <artur.barbalho@outlook.com>
2023-01-16 22:19:44 -0800

    Add local cluster with kind.
    Signed-off-by: Artur Souza <artur.barbalho@outlook.com>

---
+++ chapter-2/infra/local-terraform/kind.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    kind = {
+      source = "kyma-incubator/kind"
+      version = "0.0.11"
+    }
+  }
+}
+
+provider "kind" {
+}
+
+resource "kind_cluster" "my_cluster" {
+  name = "my-dapr-cluster"
+  kubeconfig_path = pathexpand(var.kube_config)
+  wait_for_ready = true
+  kind_config {
+    kind = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+    }
+
+    node {
+      role = "worker"
+    }
+
+    node {
+      role = "worker"
+    }
+
+    node {
+      role = "worker"
+    }
+  }
+}
