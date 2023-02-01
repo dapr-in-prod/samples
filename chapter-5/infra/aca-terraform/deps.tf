04117d2eaa52a9dcb3837f5004573eb8ec273518
Kai Walter <kai.walter@outlook.de>
2023-02-01 19:02:38 +0000

    prepare ASB split sample

---
+++ chapter-5/infra/aca-terraform/deps.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group
+  location = var.location
+  tags     = local.tags
+}
+
+module "common" {
+  source                     = "../../../common/az-deps"
+  resource_prefix            = var.resource_prefix
+  resource_group             = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  tags                       = local.tags
+  purge_protection_enabled   = var.purge_protection_enabled
+  soft_delete_retention_days = var.soft_delete_retention_days
+  secretstore_admins         = var.secretstore_admins
+}
