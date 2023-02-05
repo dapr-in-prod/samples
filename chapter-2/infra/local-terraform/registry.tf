a7aa8e6d03ed95eb25cd65dca1ea4e1710a7bbf5
Kai Walter <kai.walter@outlook.de>
2023-02-05 17:35:47 +0000

    fixes required after installing on clean machine (#5)

--- chapter-2/infra/local-terraform/registry.tf
+++ chapter-2/infra/local-terraform/registry.tf
@@ -2,16 +2,20 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-resource "docker_container" "kind-registry" {
+resource "docker_container" "kind_registry" {
   name    = "kind-registry"
   image   = "registry:2"
   restart = "always"
   ports {
-    internal = 5000
-    external = 5000
+    internal = var.registry_port
+    external = var.registry_port
     ip       = "127.0.0.1"
   }
   networks_advanced {
-    name = "kind"
+    name = docker_network.kind.name
   }
+
+  depends_on = [
+    docker_network.kind
+  ]
 }
